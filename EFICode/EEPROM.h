9944be04345d6bd134420684600bb1e8cd64ca1d
Davidoso <dayy@linux.ucla.edu>
2016-11-22 18:51:52 -0800

    Comments for writing

--- EFICode/EEPROM.h
+++ EFICode/EEPROM.h
@@ -4,6 +4,8 @@
 
 // Write up to 128 bytes of data to address. Excess values in data[] are
 // discarded.
+// There are two blocks of 64kB memory. Parameter block addresses these,
+// values 80 and 81 select the blocks.
 // Memory is divided into 128 byte pages. Writing across page boundaries
 // overflows address register and starts writing again at first byte in
 // page. Only use multiples of 128 for addresses to avoid this.
